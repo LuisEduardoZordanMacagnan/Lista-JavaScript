fd523b0cb7dd02c60ad3268f52fe9a4d3fa173f5
LuísEduardo <luiseduardozordanmacagnan@gmail.com>
2023-05-07 17:39:37 -0300

    Update 9 e 10

---
+++ Exercicio 10/script.js
@@ -0,0 +1,37 @@
+let l = document.getElementById('lista');
+let input = document.getElementById('input');
+let validacao = true;
+const array = [];
+
+function inserir() {
+    validacao = true;
+    for (var i = 0; i < array.length; i++) {
+        if (input.value == array[i]) {
+            validacao = false;
+        }
+    }
+    if (validacao) {
+        array.push(input.value);
+        listar();
+    } else {
+        window.alert('Elemento ja existente');
+    }
+}
+
+function apagar() {
+    for (var i = 0; i < array.length; i++) {
+        if (input.value == array[i]) {
+            array.splice(1);
+            listar();
+        }
+    }
+}
+
+function listar() {
+    l.innerHTML = "";
+    for (var i = 0; i < array.length; i++) {
+        p = document.createElement('p');
+        p.innerHTML = array[i];
+        l.appendChild(p);
+    }
+}
